7987488dcd21bbb62bf3f6714a270bb79e196d3e
Stefano <stefanopaulo47@gmail.com>
2019-06-27 14:39:26 -0300

    notacao literal

---
+++ aula005.js
@@ -0,0 +1,27 @@
+// Notação Literal
+
+const a = 1, b = 2, c = 3
+
+const obj1 = { a: a, b: b, c: c }
+const obj2 = { a, b, c }
+console.log(obj1, obj2)
+
+const nomeAttr = 'nota'
+const valorAttr = 7.87
+
+const obj3 = {}
+obj3[nomeAttr] = valorAttr
+console.log(obj3)
+
+const obj4 = {[nomeAttr]: valorAttr}
+console.log(obj4)
+
+const obj5 = {
+    funcao1: function(){
+        // ...
+    },
+    funcao2(){
+        // ...
+    }
+}
+console.log(obj5)
