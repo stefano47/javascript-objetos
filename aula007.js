9ebd4fbf2b1a0aadbfe9d47a96feebe55e351f76
Stefano <stefanopaulo47@gmail.com>
2019-06-27 14:50:24 -0300

    funcoes importantes de objeto

---
+++ aula007.js
@@ -0,0 +1,13 @@
+// Funções importantes de Objeto
+
+const pessoa = {
+    nome: 'Rebeca',
+    idade: 2,
+    peso: 13
+}
+
+console.log(Object.keys(pessoa))
+console.log(Object.values(pessoa))
+console.log(Object.entries(pessoa))
+
+
