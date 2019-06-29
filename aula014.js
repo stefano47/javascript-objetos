65978ba160a4578975dbdd9ec5a9e957c436bc7b
Stefano <stefanopaulo47@gmail.com>
2019-06-29 09:46:00 -0300

    evitando modificacoes

---
+++ aula014.js
@@ -0,0 +1,27 @@
+// Evitando modificações
+
+// Object.preventExtensions
+const produto = Object.preventExtensions({
+    nome: 'Qualquer', preco: 1.99, tag: 'promoção'
+})
+console.log(produto)
+
+console.log('Extensível:', Object.isExtensible(produto))
+
+produto.nome = 'Borracha'
+produto.descricao = 'Borracha escolar branca'
+delete produto.tag
+console.log(produto)
+
+// Object.seal
+const pessoa = { nome: 'Juliana', idade: 35 }
+Object.seal(pessoa)
+console.log(pessoa)
+console.log('Selado: ', Object.isSealed(pessoa))
+
+pessoa.sobrenome = 'Silva'
+delete pessoa.nome
+pessoa.idade = 29
+console.log(pessoa)
+
+// Object.freeze = selado + valores constantes
